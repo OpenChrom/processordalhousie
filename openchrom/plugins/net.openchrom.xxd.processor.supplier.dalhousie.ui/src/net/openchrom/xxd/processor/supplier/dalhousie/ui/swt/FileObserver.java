b61bbe693dbd8e35ccc94adabf58a9f9fd456b1c
liam mcnair CERC asus laptop <liam.mcnair@me.com>
2018-10-30 12:37:30 -0300

    Added FTP client functionality
    Checks the FTP server for a newer chromatogram, 
    if there is one it downloads it and displays it
    TODO: add settings for FTP server: server, user, port, password

--- openchrom/plugins/net.openchrom.xxd.processor.supplier.dalhousie.ui/src/net/openchrom/xxd/processor/supplier/dalhousie/ui/swt/FileObserver.java
+++ openchrom/plugins/net.openchrom.xxd.processor.supplier.dalhousie.ui/src/net/openchrom/xxd/processor/supplier/dalhousie/ui/swt/FileObserver.java
@@ -12,8 +12,10 @@
 package net.openchrom.xxd.processor.supplier.dalhousie.ui.swt;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 
 import org.eclipse.chemclipse.logging.core.Logger;
@@ -21,6 +23,7 @@ import org.eclipse.chemclipse.ux.extension.ui.provider.ISupplierEditorSupport;
 import org.eclipse.swt.widgets.Display;
 
 import net.openchrom.xxd.processor.supplier.dalhousie.preferences.PreferenceSupplier;
+import net.openchrom.xxd.processor.supplier.dalhousie.ftp.FtpClient;
 
 public class FileObserver {
 
@@ -28,9 +31,13 @@ public class FileObserver {
 	//
 	private ISupplierEditorSupport supplierEditorSupport;
 	private Thread thread;
+	private FtpClient ftpConnection;
+	
+	private File currentChrom;
 
 	public FileObserver(ISupplierEditorSupport supplierEditorSupport) {
 		this.supplierEditorSupport = supplierEditorSupport;
+		this.ftpConnection = new FtpClient("192.168.4.73", 21, "anonymous", "" );
 	}
 
 	public boolean isObservationRunning() {
@@ -54,6 +61,17 @@ public class FileObserver {
 	{
 		if( thread != null )
 		{
+			/* close the FTP connection */
+			try 
+			{
+				ftpConnection.close();
+			} 
+			catch(IOException e) 
+			{
+				logger.warn(e);
+			}
+			
+			/* stop the thread */
 			thread.interrupt();
 			thread = null;
 		}
@@ -66,13 +84,27 @@ public class FileObserver {
 	 */
 	private boolean startObservation() 
 	{
+		File localFileDir;
+		
 		/* Open the file specified in settings */
-		File directory = new File(PreferenceSupplier.getPathFiles());
+		localFileDir = new File(PreferenceSupplier.getPathFiles());
 		
 		/* Check the file exists */
-		if(!directory.exists())
+		if(!localFileDir.exists())
 		{
 			/* return a fail */ //TODO: warn user or throw an exception
+			return false;
+		}
+		
+		/* open the FTP connection and move to the directory */
+		try 
+		{
+			ftpConnection.open();
+			ftpConnection.changeDir("/oc_test");
+		}
+		catch(IOException e) 
+		{
+			logger.warn(e); // TODO: warn user
 			return false;
 		}
 		
@@ -82,7 +114,23 @@ public class FileObserver {
 			@Override
 			public void run()
 			{
-				runFileObserver( directory, PreferenceSupplier.getRefreshRate() );	
+				try 
+				{
+					runFileObserver( localFileDir, PreferenceSupplier.getRefreshRate() );
+				} 
+				catch(InterruptedException e) 
+				{
+					logger.warn(e);
+				}
+				
+				try 
+				{
+					ftpConnection.close();
+				}
+				catch(IOException e)
+				{
+					logger.warn(e);
+				}
 			}
 		});
 		
@@ -99,18 +147,26 @@ public class FileObserver {
 	 * - if there is one display it,
 	 * - sleep for specified amount of time
 	 * - repeat
-	 * 
-	 * 	directory: the directory to check for new chromatograms in
 	 */
-	private void runFileObserver( File directory, int refreshRate )
+	private void runFileObserver( File localFileDir, int refreshRate ) throws InterruptedException
 	{
 		File newChrom;
-		File currentChrom = null;
+		currentChrom = null;
 	
 		while(true)
 		{	
+			/* if there is a newer file in the FTP server download it to the directory */
+			try 
+			{
+				downloadNewFtpFiles(localFileDir);
+			}
+			catch(IOException e) 
+			{
+				logger.warn(e);// TODO: warn user, probably end thread or something
+			}
+			
 			/* get the newest chromatogram in the directory, checked against the current one */
-			newChrom = getNewestFile(directory, currentChrom);
+			newChrom = getNewestFile( localFileDir, currentChrom );
 			
 			/* check if a newer chromatogram was found */
 			if( newChrom != null && newChrom != currentChrom )
@@ -123,15 +179,69 @@ public class FileObserver {
 			}
 			
 			/* sleep for specified amount of time TODO: make a user setting for refresh frequency*/
-			try 
+			
+			Thread.sleep( (long) refreshRate );
+			
+		}
+	}
+	
+	/*
+	 * checks if there is a newer file on the FTP server, if there is it downloads it
+	 */
+	private boolean downloadNewFtpFiles(File localDir) throws IOException
+	{
+		String newestFtpFile;
+		File newestLocalFile;
+		boolean returnVal = false;
+
+		/* get the newest file name from FTP */
+		newestFtpFile = getNewestFileOnFTP();
+		
+		/* check if there is a file */
+		if(newestFtpFile != null)
+		{
+			/* get the newest file in the directory */
+			newestLocalFile = getNewestFile( localDir, currentChrom );
+			
+			/* check if the server has a newer file */
+			if( newestLocalFile == null  || isNewerFileName( newestLocalFile.getName(), newestFtpFile ) )
 			{
-				Thread.sleep( (long) refreshRate );
-			} 
-			catch(InterruptedException e)
-			{
-				logger.warn(e);
+				/* if it does download it */
+				ftpConnection.downloadFile( newestFtpFile, localDir.getPath() + '\\' + newestFtpFile );
+				/* indicate a file was downloaded */
+				returnVal = true;
 			}
 		}
+		
+		return returnVal;
+	}
+	
+	/*  
+	 * Get the file list on the FTP server and return the newest file name
+	 */
+	private String getNewestFileOnFTP() throws IOException
+	{
+		Collection<String> files;
+		String newestFileName = null;
+		
+		/* get the file list of the current directory */
+		files = ftpConnection.listFiles("");
+		
+		/* check all the files */
+		for( String str: files )
+		{
+			/* check the file name is in the correct format */
+			if( isCorrectChromFileFormat(str) )
+			{
+				/* check if its a newer file */
+				if( newestFileName == null || isNewerFileName(newestFileName, str) )
+				{
+					newestFileName = str;
+				}
+			}
+		}
+		
+		return newestFileName;
 	}
 	
 	/*
@@ -174,12 +284,8 @@ public class FileObserver {
 	 * return: true if newFile is newer then oldFile, false otherwise
 	 */
 	private boolean isNewerFile(File oldFile, File newFile)
-	{
-		String oldDateStr, newDateStr;
-		SimpleDateFormat parser;
-		Date oldDate, newDate;
-		
-		boolean returnVal = false;
+	{		
+		boolean returnVal;
 		
 		/* check if they are both null */
 		if(oldFile == null && newFile == null)
@@ -193,13 +299,48 @@ public class FileObserver {
 			/* it is newer */
 			returnVal = true;
 		}
+		/* if the new file is null */
+		else if(newFile == null)
+		{
+			/* its not newer */
+			returnVal = false;
+		}
 		/* check if new file is newer then old file based on the file name */
 		else
 		{
-			// TODO: check file format
+			returnVal = isNewerFileName(oldFile.getName(), newFile.getName());
+		}
+		
+		/* return the results */
+		return returnVal;
+	}
+	
+	/*
+	 * Checks if newDateStr is newer then oldDateStr
+	 */
+	private boolean isNewerFileName(String oldFileName, String newFileName)
+	{
+		SimpleDateFormat parser;
+		Date oldDate, newDate;
+		
+		boolean returnVal = false;
+		
+		/* check the format of the new file is correct */
+		if( !isCorrectChromFileFormat( newFileName ) )
+		{
+			returnVal = false;
+		}
+		/* check the format of the old file is correct */
+		else if( !isCorrectChromFileFormat( oldFileName ) )
+		{
+			returnVal = true;
+		}
+		/* get the dates in the file names */
+		else
+		{
 			/* get the date strings */
-			oldDateStr = oldFile.getName().substring(0,20);
-			newDateStr = newFile.getName().substring(0,20);
+			oldFileName = oldFileName.substring(0,20);
+			newFileName = newFileName.substring(0,20);
 			
 			/* initialize the date parser */
 			parser = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
@@ -207,20 +348,27 @@ public class FileObserver {
 			try 
 			{
 				/* parse the strings */
-				oldDate = parser.parse(oldDateStr);
-				newDate = parser.parse(newDateStr);
+				oldDate = parser.parse(oldFileName);
+				newDate = parser.parse(newFileName);
 				
 				/* check if it is newer */
 				returnVal = newDate.after(oldDate);
-			} 
+			}
 			catch(ParseException e)
 			{
 				logger.warn(e);
 			}
 		}
 		
-		/* return the resutls */
 		return returnVal;
+	}
+	
+	/* 
+	 * Checks file format to be yyyy_MM_dd_HH_mm_ss_chromatogram.xy
+	 */
+	private boolean isCorrectChromFileFormat(String str)
+	{
+		return str != null && str.matches("\\d{4}_\\d{2}_\\d{2}_\\d{2}_\\d{2}_\\d{2}_chromatogram.xy");
 	}
 	
 	/*
---
+++ openchrom/plugins/net.openchrom.xxd.processor.supplier.dalhousie/src/net/openchrom/xxd/processor/supplier/dalhousie/ftp/FtpClient.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2018 oceancerc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * oceancerc - initial API and implementation
+*******************************************************************************/
+package net.openchrom.xxd.processor.supplier.dalhousie.ftp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class FtpClient {
+
+    private final String server;
+    private final int port;
+    private final String user;
+    private final String password;
+    private FTPClient ftp;
+
+    public FtpClient(String server, int port, String user, String password) {
+        this.server = server;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+    }
+
+    public void open() throws IOException {
+        ftp = new FTPClient();
+
+        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+
+        ftp.connect(server, port);
+        int reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new IOException("Exception in connecting to FTP Server");
+        }
+
+        ftp.login(user, password);
+    }
+
+    public void close() throws IOException {
+        ftp.disconnect();
+    }
+
+    public Collection<String> listFiles(String path) throws IOException {
+        FTPFile[] files = ftp.listFiles(path);
+
+        return Arrays.stream(files)
+                .map(FTPFile::getName)
+                .collect(Collectors.toList());
+    }
+
+    public void putFileToPath(File file, String path) throws IOException {
+        ftp.storeFile(path, new FileInputStream(file));
+    }
+
+    public void downloadFile(String source, String destination) throws IOException {
+        FileOutputStream out = new FileOutputStream(destination);
+        ftp.retrieveFile(source, out);
+    }
+    
+    public boolean changeDir(String path) throws IOException
+    {
+    	return ftp.changeWorkingDirectory(path);
+    }
+}
