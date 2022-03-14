4bdbb0062aa95d4cc0c754edf912a520c0e10965
Philip Wenig <philip.wenig@openchrom.net>
2022-03-14 15:13:22 +0100

    Reduce Compiler Warnings

--- openchrom/plugins/net.openchrom.xxd.processor.supplier.dalhousie.fragment.test/src/net/openchrom/xxd/processor/supplier/dalhousie/TestPathHelper.java
+++ openchrom/plugins/net.openchrom.xxd.processor.supplier.dalhousie.fragment.test/src/net/openchrom/xxd/processor/supplier/dalhousie/TestPathHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Lablicate GmbH.
+ * Copyright (c) 2018, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,8 +10,6 @@
  * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package net.openchrom.xxd.processor.supplier.dalhousie;
-
-import net.openchrom.xxd.processor.supplier.dalhousie.PathResolver;
 
 /**
  * THIS CLASS IS NOT SUITED FOR PRODUCTIVE USE!<br/>
